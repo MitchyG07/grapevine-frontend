43be767b3e8b8e8563e7e6d2f50152b3e22bcaa1
wcharlesknight <williamchknight@gmail.com>
2021-02-11 16:43:54 -0800

    start of frontend

---
+++ src/components/Login.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+
+function Login (props){
+
+    return(
+
+        <div>
+        <form>
+          <label for="fname">First name:</label>
+          <input type="text" id="fname" name="fname"/>
+          <label for="lname"> Last name:</label> 
+          <input type="text" id="lname" name="lname"/>
+        </form> 
+        </div>
+
+    )
+
+}
+
+export default Login 
---
+++ src/containers/MainContainer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Login from '../components/Login'
+
+
+
+export default class MainContainer extends React.Component {
+  
+  render() {
+   return (
+    
+    <Login /> 
+  
+   )
+  } 
+}
