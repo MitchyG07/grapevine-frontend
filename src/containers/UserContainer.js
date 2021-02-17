1e63f5bd1cd0e95968e532a7a1c153fe0a445d63
wcharlesknight <williamchknight@gmail.com>
2021-02-17 13:58:58 -0800

    pushin

--- src/containers/UserContainer.js
+++ src/containers/UserContainer.js
@@ -5,7 +5,9 @@ class UserContainer extends Component {
     state = {
         currentUser: this.props.user,
         bio: '',
-        name: ''
+        name: '',
+        favorites: [],
+        reviews: []
     }
 
 
@@ -13,44 +15,62 @@ componentDidMount(){
     this.getUser()
 }
 
+
+
 setUser = (user) => {
-    this.setState({
+    user.wines.forEach( wines  => this.setState({
        name: user.username,
-       bio: user.bio
-    })
-}
+       bio: user.bio,
+       favorites: [...this.state.favorites, wines.title]
+    }))
+    const token = localStorage.token;
+    let  configW = {method: 'GET',  
+    headers: {Authorization:  `Bearer ${token}`}} 
+    user.reviews.forEach(  reviews => {
+        fetch(`http://localhost:3000/wines/${reviews.wine_id}`, configW)
+        .then(resp =>  resp.json())
+        .then(wine =>  this.setState({
+            reviews: [...this.state.reviews, {content: reviews.content, wine: wine.title}] })) })
+    }
 
 getUser =  () => {
     const token = localStorage.token;
-    let  configObj = {type: 'GET',  
+    let  configObj = {method: 'GET',  
     headers: {Authorization:  `Bearer ${token}`}} 
-   
-     fetch(`http://localhost:3000/users/${localStorage.id}`, configObj) 
+fetch(`http://localhost:3000/users/${localStorage.id}`, configObj) 
     .then(resp  => resp.json())
     .then(user => this.setUser(user))  
+    }
+
+handleSubmit = (e) => {
+    e.preventDefault()
+    this.setState({
+        bio:  e.target.children[1].value 
+    }) 
+    this.editBio()
 }
 
-editBio = (e) => {
-    e.preventDefault()
+editBio = () => {
+    const bio = {bio: this.state.bio}
     const token = localStorage.token;
     const user_id = localStorage.id
     fetch(`http://localhost:3000/users/${user_id}`,  {
-    type: 'PATCH',
-    headers: {
-        Authorization: `Bearer ${token}`
-    }, body: {
-        bio: e.target.children[1].value
-    } }
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`
+    }, body: 
+       JSON.stringify(bio)
+     }
     ) 
         .then(resp => resp.json())
-        .then(bio => console.log(bio))
-    
+        .then(user => this.setUser(user))
         }
-// }
+
         
         
 
-render(){
+render() {
     console.log(this.props.user)
    
     const {username,wines, reviews} = this.state.currentUser
@@ -59,20 +79,22 @@ render(){
         <div>
            <a>{this.state.name}</a>
            <a>{this.state.bio}</a>
-           <form onSubmit={(e) => this.editBio(e)}> 
+           <form onSubmit={(e) => this.handleSubmit(e)}> 
               Bio: <br></br>
             <textarea></textarea>
             <input type='submit' value='Submit'></input>
            </form>
-           <a>{wines}</a>
-           <a>{reviews}</a>
+           <ol> 
+               {this.state.favorites.map(faves => <li>{faves}</li>)} 
+           </ol>
+            <ul> 
+                {this.state.reviews.map(review => <li>{review.content} -- {review.wine}</li>)} 
+            </ul>
+           
         </div>
     )
 
     }
-
-   
 }
-
 
 export default UserContainer
